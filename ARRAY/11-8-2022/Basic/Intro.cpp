cc0adb7e9000e480c30da7a521256c050ed3d5d8
LOVKUSH <lovkushyadav9888@gmail.com>
2022-11-08 22:37:01 +0530

    Taking Input from user in array

--- ARRAY/11-8-2022/Basic/Intro.cpp
+++ ARRAY/11-8-2022/Basic/Intro.cpp
@@ -1,9 +1,11 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-main(){
-    int a[] = {1,2,3,4,5};
+main()
+{
+    int a[] = {1, 2, 3, 4, 5};
     int size = 5;
-    for (int i = 0; i < size; i++) {
-        cout<<a[i];
+    for (int i = 0; i < size; i++)
+    {
+        cout << a[i];
     }
 }
