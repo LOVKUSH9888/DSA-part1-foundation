1d274ad7272065ce4516b9e72e9d534374fd262b
LOVKUSH <lovkushyadav9888@gmail.com>
2022-11-09 22:05:04 +0530

    2d Array

---
+++ ARRAY/2D_ARRAY_Lecture 2/basics.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+main(){
+    //Synatax
+    int arr[rows][column];   //[] = Rows & 2nd [] = column
+    int arr [2][2]; // rows = 2 & column = 2
+    int arr [2][3];  // rows = 2 & column = 3
+    //Initialize
+    int arr[2][2] = {1,2,3,4};
+    int arr[2][2] = {1,2},{3,4};
+}
