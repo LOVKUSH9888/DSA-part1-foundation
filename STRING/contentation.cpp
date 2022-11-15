54abacba3fb9dbf1d494eab51cf11b6eb5c2be44
LOVKUSH <lovkushyadav9888@gmail.com>
2022-11-16 00:02:16 +0530

    Contentation in String

---
+++ STRING/contentation.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+    string name;
+    cout<<"Enter a valid name here";
+    cin>>name;
+    name = name + "vinod kumar";
+    cout<<"Entered name is "<< name;
+}
