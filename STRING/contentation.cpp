52e08be019dcdbe8da75c4ea57e9b3c5cfc500e7
LOVKUSH <lovkushyadav9888@gmail.com>
2022-11-16 00:10:18 +0530

    push_back + length of string

--- STRING/contentation.cpp
+++ STRING/contentation.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
+// concat +append { something add }
 using namespace std;
-int main(){
+int main()
+{
     string name;
-    cout<<"Enter a valid name here";
-    cin>>name;
+    cout << "Enter a valid name here";
+    cin >> name;
     name = name + "vinod kumar";
-    cout<<"Entered name is "<< name;
+    cout << "Entered name is " << name;
+
+    // we can also do this same thing by name.pus_back('l');
+    // use this metod as this is most time complexity
+}
+
+// Using the length functions
+main()
+{
+    string name = "Lovkush";
+    cout << name.length() << endl;
 }
